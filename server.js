f0e1ca1026118e7707e390222686db8516c19e83
Raudel Garcia <raudelgar86@raudels-mini.home>
2019-07-04 15:07:27 -0400

    adding mock data, testing server rest call

--- server.js
+++ server.js
@@ -4,8 +4,17 @@ const port = process.env.PORT || 5000;
 const fs = require('fs');
 const path = require('path');
 
+//Mock Data
+const data17 = require('./mockData/data-2017.json');
+
+
+app.get('/dashboard/health',(req,res)=>{
+    res.send('Hello World!!');
+});
+
 //Adding JSON Middleware
 app.use(express.json());
+
 
 
 //Listen Port
