2c493a00031ffa90e6d6cbc27b719a78545f8995
Raudel Garcia <raudelgar86@raudels-mini.home>
2019-07-05 21:30:56 -0400

    working on global tooltip

---
+++ public/scripts/view/tooltip.js
@@ -0,0 +1,70 @@
+const tooltip = {
+    el: document.body.querySelector('tooltip'),
+    marginOffset: {
+        left: 10,
+        top: -25
+    },
+    init: function(){
+        contentEl.addEventListener('mousemove',tooltip.mousemove);
+        tooltip.el.style.margin = `${tooltip.marginOffset.top}px 0 0 ${tooltip.marginOffset.left}px`;
+    },
+    mousemove: function(e){
+        tooltip.el.style.left = `${e.clientX}px`;
+        tooltip.el.style.top = `${e.clientY + contentEl.scrollTop}px`;
+
+        let headerHeight = 50;
+        let tooltipWidth = tooltip.el.offsetWidth;
+        let tooltipHeight = tooltip.el.offsetHeight;
+        let scrollbarXWidth = contentEl.offsetHeight < contentEl.scrollHeight ? 17 : 0;
+        let scrollbarYWidth = contentEl.offsetWidth < contentEl.scrollWidth ? 17 : 0;
+        let marginLeft = tooltip.marginOffset.left;
+        let marginTop  = tooltip.marginOffset.top;
+
+        if(e.clientX >= document.body.clientWidth - scrollbarXWidth - tooltipWidth - tooltip.marginOffset.left){
+            marginLeft = -tooltipWidth + tooltip.marginOffset.left;
+        }
+
+        if(e.clientX >= document.body.clientHeight - scrollbarYWidth - tooltipHeight + tooltip.marginOffset.top){
+            marginTop = -headerHeight - tooltipHeight - 5;
+        }
+
+        tooltip.el.style.margin = `${marginTop}px 0 0 ${marginLeft}px`;
+    },
+    hide: function(){
+        tooltip.el.removeAttribute('active');
+    },
+    toggled: function(el){
+        tooltip.el.setAttribute('class',el.getAttribute('class'.includes('toggled') ? 'toggled': ''));
+    },
+    update: function(o){
+        let s = '';
+        tooltip.el.setAttribute('active','');
+
+        switch(o.meta.type){
+            case 'bar':
+                s = `<items><item><property>${o.data.label}:</property><value>${o.meta.pre}${o.data.value}${o.data.post}</value></item></items>`;
+                break;
+            case 'barLine':
+                let pre = o.meta.pre || '';
+                let post  = o.meta.post || '';
+
+                s += `<item><property>${o.data.dataBar[0].key}:</property><value>${pre}${o.data.dataBar[0].value}${post}</value></item>`;
+                s += `<item><property>${o.data.dataLine[0].key}:</property><value>${pre}${o.data.dataLine[0].value}${post}</value></item>`;
+
+                s = `<title>${o.data.label}</title><items>${s}</items>`;
+                break;
+            case 'donut':
+                s = `<items><item><property>${o.data.label}:</property><value>${o.meta.pre}${o.data.value}${o.data.post}</value></item></items>`;
+                break;
+            case 'gropuedBar':
+                for(let k in o.data.values){
+                    if(o.data.values[k]){
+                        s += `<item><property>${k}:</property><value>${o.data.values[k]}</value></item>`;
+                    }
+                }
+                s = `<title>${o.data.label}</title><items>${s}</items>`; 
+                break;
+            
+        }
+    }
+}
